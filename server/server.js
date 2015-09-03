23691cb777df3c7e9ae0c9b7f073175ce355de35
Adam <adam.l.guerra@gmail.com>
2015-09-03 15:32:10 -0500

    removed some comments.

--- server/server.js
+++ server/server.js
@@ -8,9 +8,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.listen(8000, function() {console.log('Running on port 8000.')});
 
-// setInterval(function(){console.log('still going.')},1500)
-console.log(__dirname)
-
 //routes
 addRoutes(app);
 
@@ -18,8 +15,6 @@ addRoutes(app);
 app.use(express.static('./client'));
 app.use(express.static('./bower_components'));
 
-// misc. thoughts:
-// 	have api endpoints for regular swipe functionality and receiving/storing preferences.
-// other possibilities:
-// 	endpoint to return/render previous answers - need actual database with users and ratings.
-// 	endpoint to buy stuff - would probably just use stripe on the client side, anyway.
+// other endpoint possibilities:
+// 	endpoint to return/render previously rated pieces - need database with users and ratings.
+// 	endpoint to buy stuff - would probably just use stripe on the client side, but need to update database.
